c176dc21f4c54886bbff450a90bc0d41bec6f6d3
tianxin5750 <xin.tian@megatronix.co>
2023-07-28 19:16:19 +0800

    add not

--- modules/planning/lattice/behavior/prediction_querier.h
+++ modules/planning/lattice/behavior/prediction_querier.h
@@ -33,16 +33,14 @@ namespace planning {
 class PredictionQuerier {
  public:
   PredictionQuerier(const std::vector<const Obstacle*>& obstacles,
-                    const std::shared_ptr<std::vector<common::PathPoint>>&
-                        ptr_reference_line);
+  const std::shared_ptr<std::vector<common::PathPoint>>& ptr_reference_line);
 
   virtual ~PredictionQuerier() = default;
 
   std::vector<const Obstacle*> GetObstacles() const;
 
-  double ProjectVelocityAlongReferenceLine(const std::string& obstacle_id,
-                                           const double s,
-                                           const double t) const;
+  double ProjectVelocityAlongReferenceLine(const std::string& obstacle_id,const double s,
+  const double t) const;
 
  private:
   std::unordered_map<std::string, const Obstacle*> id_obstacle_map_;
